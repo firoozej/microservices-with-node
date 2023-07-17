48692673e4837442f7a0f7f45b97ffb64901136d
firoozej <firoozej2@gmail.com>
2023-07-17 16:25:20 +0200

    feat(tickets): show/update tickets

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -6,6 +6,8 @@ import cookieSession from "cookie-session"
 import { currentUser, errorHandler, NotFoundError } from "@firoozej/ticketingshared"
 import { createTicketRouter } from "./routes/new"
 import { showTicketRouter } from "./routes/show"
+import { indexTicketRouter } from "./routes/index"
+import { updateTicketRouter } from "./routes/update"
 
 const app = express()
 app.set("trust proxy", true)
@@ -20,6 +22,8 @@ app.use(currentUser);
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all("*", async () => {
     throw new NotFoundError()
