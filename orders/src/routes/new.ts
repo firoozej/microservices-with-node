34a583e25ad46adeb09aa0d6669b62288720b1aa
firoozej <firoozej2@gmail.com>
2023-08-04 15:25:51 +0200

    feat(orders): publish/listening to events

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -59,12 +59,13 @@ router.post(
     // Publish an event saying that an order was created
     new OrderCreatedPublisher(natsWrapper.client).publish({
       id: order.id,
+      version: order.version,
       status: order.status,
       userId: order.userId,
       expiresAt: order.expiresAt.toISOString(),
       ticket: {
         id: ticket.id,
-        price: ticket.price,
+        price: ticket.price
       },
     });
 
